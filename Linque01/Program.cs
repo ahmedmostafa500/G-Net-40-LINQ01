35ea2f8089f9a4fbf3b8d85408d185cad2c04f96
ahmedmostafa500 <plmncar500@gmail.com>
2026-04-06 14:35:01 +0200

    question6

--- Linque01/Program.cs
+++ Linque01/Program.cs
@@ -57,17 +57,31 @@
             //        }
             #endregion
             #region question5
-    //        var availableCondiments = ProductList
-    //.Where(p => p.UnitsInStock > 0 && p.Category == "Condiments")
+            //        var availableCondiments = ProductList
+            //.Where(p => p.UnitsInStock > 0 && p.Category == "Condiments")
+            //.Select(p => new
+            //{
+            //    Name = p.ProductName,
+            //    Stock = p.UnitsInStock
+            //});
+
+            //        foreach (var product in availableCondiments)
+            //        {
+            //            Console.WriteLine($"Name: {product.Name}, Stock: {product.Stock}");
+            //        }
+            #endregion
+            #region question6
+    //        var result = ProductList
     //.Select(p => new
     //{
     //    Name = p.ProductName,
-    //    Stock = p.UnitsInStock
+    //    Price = p.UnitPrice,
+    //    StockStatus = p.UnitsInStock > 0 ? "Available" : "Out of Stock"
     //});
 
-    //        foreach (var product in availableCondiments)
+    //        foreach (var item in result)
     //        {
-    //            Console.WriteLine($"Name: {product.Name}, Stock: {product.Stock}");
+    //            Console.WriteLine($"Name: {item.Name}, Price: {item.Price}, Status: {item.StockStatus}");
     //        }
             #endregion
         }
