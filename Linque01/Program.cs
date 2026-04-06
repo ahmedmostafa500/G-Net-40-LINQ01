492d28d8e6a19227e0e8b05826b9e29688b53d20
ahmedmostafa500 <plmncar500@gmail.com>
2026-04-06 14:25:02 +0200

    question1

--- Linque01/Program.cs
+++ Linque01/Program.cs
@@ -4,7 +4,44 @@
     {
         static void Main(string[] args)
         {
-           
+            #region question1
+            //        var seafoodProducts = products
+            //.Where(p => p.Category == "Seafood")
+            //.Select(p => new
+            //{
+            //    Name = p.ProductName,
+            //    Price = p.UnitPrice
+            //});
+
+            //        foreach (var product in seafoodProducts)
+            //        {
+            //            Console.WriteLine($"Name: {product.Name}, Price: {product.Price}");
+            //        }
+            #endregion
+            #region question2
+            //        var productNames = ProductList
+            //.Select(p => p.ProductName);
+
+            //        foreach (var name in productNames)
+            //        {
+            //            Console.WriteLine(name);
+            //        }
+            #endregion
+            #region question3
+        //    var sortedProducts = ProductList
+        //.OrderBy(p => p.UnitPrice)
+        //.Select(p => new
+        //{
+        //    Name = p.ProductName,
+        //    Price = p.UnitPrice
+        //});
+
+        //    foreach (var product in sortedProducts)
+        //    {
+        //        Console.WriteLine($"Name: {product.Name}, Price: {product.Price}");
+        //    }
+
+            #endregion
         }
     }
 }
